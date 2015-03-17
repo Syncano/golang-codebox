0ee9116db253efe8a118d4d9d238f9b9e2e650d2
Justyna Ilczuk <justyna.ilczuk@gmail.com>
2015-03-17 16:05:47 +0100

    Update base.go example

--- base.go
+++ base.go
@@ -9,10 +9,11 @@ import "fmt"
 
 // don't forget to include main function
 func main() {
-        config, args := initialize()
+        config, args, meta := initialize()
         fmt.Println("Hello from go!")
         fmt.Println(config)
         fmt.Println(args)
+        fmt.Println(meta)
         concurrency := config["concurrency"].(float64)
         fmt.Println(concurrency)
 }
@@ -21,12 +22,15 @@ func main() {
 
 // helper that will parse codebox config and args for you
 
-func initialize() (map[string]interface{}, map[string]interface{}) {
+func initialize() (map[string]interface{}, map[string]interface{}, map[string]interface{}) {
         config_raw := []byte(`{"apiKey":"xxxxxxxx", "concurrency":4}`)
-        args_raw := []byte(`{"this":"That", "other": 42}`)
+        args_raw := []byte(`{"this":"That", "other":42}`)
+        meta_raw := []byte(`{"executed_by": "webhook", "executor": "webhook_slug"}`)
         var config map[string]interface{}
         var args map[string]interface{}
+        var meta map[string]interface{}
         json.Unmarshal(config_raw, &config)
         json.Unmarshal(args_raw, &args)
-        return config, args
+        json.Unmarshal(meta_raw, &meta)
+        return config, args, meta
 }
