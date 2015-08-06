f364ab7e1f9d017da6adf797981484990bb8c8e4
Robert Kopaczewski <rk@23doors.com>
2015-08-06 17:47:31 +0200

    Update base.go

--- base.go
+++ base.go
@@ -25,7 +25,7 @@ var ARGS = _unmarshall_json([]byte(`{"this":"That", "other":42}`))
 var META = _unmarshall_json([]byte(`{"executed_by": "webhook", "executor": "webhook_slug"}`))
 var CONFIG = _unmarshall_json([]byte(`{"apiKey":"xxxxxxxx", "concurrency":4}`))
 
-func _unmarshall_json_string(data_raw []byte) map[string]interface{{}} {{
+func _unmarshall_json(data_raw []byte) map[string]interface{{}} {{
     var data map[string]interface{{}}
     json.Unmarshal(data_raw, &data)
     return data
